53020f9b1ae92a0e235a2b6cd7f405e88b1a3bbc
Michael Watson <m.watson.tn@gmail.com>
2017-03-25 21:49:13 -0500

    installed moment

--- express-hello-world.js
+++ express-hello-world.js
@@ -3,7 +3,23 @@
 const express = require('express');
 const app = express();
 
+const requestTime = (req, res, next) => {
+	req.requestedTime = Date.now();
+	let newDate = req.requestedTime.toISOString();
+	next();
+}
+
 app.use(express.static(__dirname + '/'));
+app.use(requestTime);
+
+app.get('/', (req, res, next) => {
+	res.send('hello world')
+})
+
+app.get('/time', (req, res, next) => {
+	res.send(`Run at ${newDate}`);
+})
+
 
 
 app.listen(8080, () => {
